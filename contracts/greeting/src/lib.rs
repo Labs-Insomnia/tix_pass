4e5297b4d2d751a2f53559168f4ae877925d2de4
= <=>
2024-07-13 23:37:56 +0100

    prelim added

--- contracts/greeting/src/lib.rs
+++ contracts/greeting/src/lib.rs
@@ -30,6 +30,18 @@ impl BaseContract {
         salt: BytesN<32>,
         init_fn: Symbol,
         init_args: Vec<Val>, ){
+            // Skip authorization if deployer is the current contract.
+        if deployer != env.current_contract_address() {
+            deployer.require_auth();
+        }
+        // Deploy the contract using the uploaded Wasm with given hash.
+        let deployed_address = env
+            .deployer()
+            .with_address(deployer, salt)
+            .deploy(wasm_hash);
+
+         // Invoke the init function with the given arguments.
+         let res: Val = env.invoke_contract(&deployed_address, &init_fn, init_args);
 
     }
     
