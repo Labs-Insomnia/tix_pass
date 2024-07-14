baad70831c681fab2d88798a737afac0d43712a3
= <=>
2024-07-14 15:06:32 +0100

    hello world

--- contracts/tix_contract/src/lib.rs
+++ contracts/tix_contract/src/lib.rs
@@ -1,14 +1,17 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+#![no_std]
+use soroban_sdk::{contract, contractimpl, Env, Symbol, symbol_short, String,Vec,};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[contract]
+pub struct TixContract;
+
+
+impl TixContract{
+    pub fn init(){
+
+    }
+    pub fn transfer_tix() {
+        
     }
 }
+
